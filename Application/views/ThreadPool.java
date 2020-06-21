1a47a2766e30aa735aff8d4533a903ff3370c2bd
Linus Kadner <i19024@hb.dhbw-stuttgart.de>
2020-06-21 15:48:07 +0200

    Simple Threadpool

---
+++ Application/views/ThreadPool.java
@@ -0,0 +1,11 @@
+package views;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadPool {
+	ExecutorService executor = Executors.newCachedThreadPool();
+	
+	ThreadPoolExecutor pool = (ThreadPoolExecutor) executor;
+}
